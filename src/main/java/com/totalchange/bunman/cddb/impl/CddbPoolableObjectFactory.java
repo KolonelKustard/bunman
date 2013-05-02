01fbede1fb697a46e6768dd4fb20de7b3f80bb39
kolonel <ralph@totalchange.com>
2013-05-02 22:33:16 +0100

    Added looking up of product name and version from manifest

--- src/main/java/com/totalchange/bunman/cddb/impl/CddbPoolableObjectFactory.java
+++ src/main/java/com/totalchange/bunman/cddb/impl/CddbPoolableObjectFactory.java
@@ -4,20 +4,35 @@ import org.apache.commons.pool.BasePoolableObjectFactory;
 
 import com.samskivert.net.cddb.CDDB;
 
-final class CddbPoolableObjectFactory extends
-        BasePoolableObjectFactory<CDDB> {
+final class CddbPoolableObjectFactory extends BasePoolableObjectFactory<CDDB> {
+    private static final String DEFAULT_CLIENT_NAME = "Bunman";
+    private static final String DEFAULT_CLIENT_VERSION = "1.0";
+
     private String hostname;
     private int port;
+    private String clientName;
+    private String clientVersion;
 
     CddbPoolableObjectFactory(String hostname, int port) {
         this.hostname = hostname;
         this.port = port;
+
+        this.clientName = this.getClass().getPackage().getImplementationTitle();
+        if (this.clientName == null || this.clientName.length() <= 0) {
+            this.clientName = DEFAULT_CLIENT_NAME;
+        }
+
+        this.clientVersion = this.getClass().getPackage()
+                .getImplementationVersion();
+        if (this.clientVersion == null || this.clientVersion.length() <= 0) {
+            this.clientVersion = DEFAULT_CLIENT_VERSION;
+        }
     }
 
     @Override
     public CDDB makeObject() throws Exception {
         CDDB cddb = new CDDB();
-        cddb.connect(hostname, port, "Bunman", "1.0");
+        cddb.connect(hostname, port, clientName, clientVersion);
         return cddb;
     }
 
