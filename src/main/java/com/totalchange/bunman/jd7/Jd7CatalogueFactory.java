e3f8a7db04226d4796853ee18797f6e67ddb7218
kolonel <ralph@totalchange.com>
2013-05-15 17:55:21 +0100

    Sorting the location bits

--- src/main/java/com/totalchange/bunman/jd7/Jd7CatalogueFactory.java
+++ src/main/java/com/totalchange/bunman/jd7/Jd7CatalogueFactory.java
@@ -1,6 +1,7 @@
 package com.totalchange.bunman.jd7;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -20,8 +21,28 @@ public class Jd7CatalogueFactory implements CatalogueFactory {
     }
 
     public List<Location> findDefaultLocations() {
-        // TODO Auto-generated method stub
-        return null;
+        List<Location> locations = new ArrayList<Location>();
+
+        File[] roots = File.listRoots();
+        for (File root : roots) {
+            File dir = new File(root, "hardfi");
+            dir = new File(dir, "music");
+
+            if (dir.exists() && dir.isDirectory()) {
+                final File backupDir = new File(dir.getAbsolutePath());
+                locations.add(new Location() {
+                    public String getName() {
+                        return "Brennan JD7 Backup";
+                    }
+
+                    public File getRoot() {
+                        return backupDir;
+                    }
+                });
+            }
+        }
+
+        return locations;
     }
 
     public Catalogue createCatalogue(File root) {
--- src/main/java/com/totalchange/bunman/library/LibraryCatalogueFactory.java
+++ src/main/java/com/totalchange/bunman/library/LibraryCatalogueFactory.java
@@ -1,6 +1,7 @@
 package com.totalchange.bunman.library;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.totalchange.bunman.Catalogue;
@@ -8,12 +9,72 @@ import com.totalchange.bunman.CatalogueFactory;
 
 public final class LibraryCatalogueFactory implements CatalogueFactory {
     public List<Location> findDefaultLocations() {
-        // TODO Auto-generated method stub
-        return null;
+        List<Location> locations = new ArrayList<Location>();
+
+        String usersDirStr = System.getProperty("user.home");
+        if (usersDirStr != null) {
+            File usersDir = new File(usersDirStr);
+
+            // iTunes - Windows XP
+            File dir = new File(usersDir, "My Documents");
+            dir = new File(dir, "My Music");
+            dir = new File(dir, "iTunes");
+            dir = new File(dir, "iTunes Media");
+            dir = new File(dir, "Music");
+            if (dir.exists() && dir.isDirectory()) {
+                final File itunesLibrary = new File(dir.getAbsolutePath());
+                locations.add(new Location() {
+                    public String getName() {
+                        return "iTunes Music Library";
+                    }
+
+                    public File getRoot() {
+                        return itunesLibrary;
+                    }
+                });
+            }
+
+            // iTunes - Windows 7 & 8
+            dir = new File(usersDir, "My Music");
+            dir = new File(dir, "iTunes");
+            dir = new File(dir, "iTunes Media");
+            dir = new File(dir, "Music");
+            if (dir.exists() && dir.isDirectory()) {
+                final File itunesLibrary = new File(dir.getAbsolutePath());
+                locations.add(new Location() {
+                    public String getName() {
+                        return "iTunes Music Library";
+                    }
+
+                    public File getRoot() {
+                        return itunesLibrary;
+                    }
+                });
+            }
+
+            // iTunes - Mac and Windows Vista
+            dir = new File(usersDir, "Music");
+            dir = new File(dir, "iTunes");
+            dir = new File(dir, "iTunes Media");
+            dir = new File(dir, "Music");
+            if (dir.exists() && dir.isDirectory()) {
+                final File itunesLibrary = new File(dir.getAbsolutePath());
+                locations.add(new Location() {
+                    public String getName() {
+                        return "iTunes Music Library";
+                    }
+
+                    public File getRoot() {
+                        return itunesLibrary;
+                    }
+                });
+            }
+        }
+
+        return locations;
     }
 
     public Catalogue createCatalogue(File root) {
-        // TODO Auto-generated method stub
-        return null;
+        return new LibraryCatalogue(root);
     }
 }
